cc4e93f9dc1c20c9c080b72b508d453ddaab278e
Shivanshu <your.email@example.comgit>
2025-06-26 14:52:02 +0530

    basic structure

---
+++ src/main/java/com/example/going_green/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.going_green;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.going_green {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.going_green to javafx.fxml;
+    exports com.example.going_green;
+}
